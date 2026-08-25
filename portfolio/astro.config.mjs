bacff4b0e478a207f9a3ae51c7cdc173561db312
WarquahF <warquah.dev@proton.me>
2026-08-25 20:27:25 +0530

    Update domain to warquahf.vercel.app for sitemap and robots.txt

--- portfolio/astro.config.mjs
+++ portfolio/astro.config.mjs
@@ -3,7 +3,7 @@ import svelte from '@astrojs/svelte';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://warquahf.netlify.app',
+  site: 'https://warquahf.vercel.app',
   integrations: [svelte(), sitemap()],
   output: 'static',
   compressHTML: true,
